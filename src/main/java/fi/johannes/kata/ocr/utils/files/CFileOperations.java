045efa2789282198fa3b13400ccc74b48d3d2d23
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-07-01 15:06:44 +0300

    Added some file utils from other project and tests

---
+++ src/main/java/fi/johannes/kata/ocr/utils/files/CFileOperations.java
@@ -0,0 +1,312 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2016 Johannes Sarpola.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fi.johannes.kata.ocr.utils.files;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * TODO Test at some point
+ * @author Johannes töissä
+ */
+public class CFileOperations {
+
+    public static String fileContentToString(String filepath) throws IOException {
+        Path p = Paths.get(filepath);
+        byte[] contents = Files.readAllBytes(p);
+        return new String(contents, StandardCharsets.UTF_8);
+    }
+
+    public static List<byte[]> getFileContentAsBytes(String f) throws IOException {
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        List<byte[]> res = getFileContentAsBytes(br);
+        fr.close();
+        return res;
+    }
+
+    public static List<byte[]> getFileContentAsBytes(File f) throws IOException {
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        List<byte[]> res = getFileContentAsBytes(br);
+        fr.close();
+        return res;
+    }
+
+    public static List<char[]> getFileContentAsChars(File f) throws IOException {
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        List<char[]> res = getFileContentAsChars(br);
+        fr.close();
+        return res;
+    }
+
+    public static List<char[]> getFileContentAsChars(String f) throws IOException {
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        List<char[]> res = getFileContentAsChars(br);
+        fr.close();
+        return res;
+    }
+
+    private static ArrayList<char[]> getFileContentAsChars(BufferedReader br) throws IOException {
+        ArrayList<char[]> cList = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            cList.add(line.toCharArray());
+        }
+        return cList;
+    }
+
+    public static List<String> getFileContentAsStrings(Path f) throws IOException {
+        FileReader fr = new FileReader(f.toString());
+        BufferedReader br = new BufferedReader(fr);
+        List<String> res = getFileContentAsStrings(br);
+        fr.close();
+        return res;
+    }
+
+    public static List<String> getFileContentAsStrings(File f) throws IOException {
+        FileReader fr = new FileReader(f.toString());
+        BufferedReader br = new BufferedReader(fr);
+        List<String> res = getFileContentAsStrings(br);
+        fr.close();
+        return res;
+    }
+
+    public static List<String> getFileContentAsStrings(String f) throws IOException {
+        FileReader fr = new FileReader(f.toString());
+        BufferedReader br = new BufferedReader(fr);
+        List<String> res = getFileContentAsStrings(br);
+        fr.close();
+        return res;
+    }
+
+    private static ArrayList<String> getFileContentAsStrings(BufferedReader br) throws IOException {
+        ArrayList<String> sList = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sList.add(line);
+        }
+        br.close();
+        return sList;
+    }
+
+    private static ArrayList<byte[]> getFileContentAsBytes(BufferedReader br) throws IOException {
+        ArrayList<byte[]> bList = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            bList.add(line.getBytes(StandardCharsets.UTF_8));
+        }
+
+        br.close();
+        return bList;
+    }
+
+    public static int countLines(File f) throws FileNotFoundException, IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(f));
+        return countLines(is);
+    }
+
+    public static int countLines(String filepath) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(filepath));
+        return countLines(is);
+    }
+
+    public static int countLines(InputStream is) throws IOException {
+        try {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } finally {
+            is.close();
+        }
+    }
+
+
+/*
+    public static File[] getFilesInFolder(String path) {
+        //TODO Throw exception if it's not folder
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        return listOfFiles;
+    }
+
+    public static List<String> getFilenamesInFolder(String path) {
+        File[] files = getFilesInFolder(path);
+        List<String> names = new ArrayList<>();
+        for (File file : files) {
+            if (file.isFile()) {
+                names.add(file.getName());
+            }
+        }
+        return names;
+    }
+*/
+    public static void createChunksByRowsAndBytes(String inputFile, String outputFolder, int bufferSize, int rows) throws FileNotFoundException, IOException {
+        File f = new File(inputFile);
+        String filename = f.getName();
+        BufferedReader bw = new BufferedReader(new FileReader(f));
+        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        int j = 0;
+        for (int i = 0; i <= rows; i++) {
+            String lineStr = bw.readLine();
+            // TODO Process each line
+            if (lineStr != null) {
+                byte[] line = lineStr.getBytes(StandardCharsets.UTF_8);
+                if (i == rows) {
+                    String outputfile = outputFolder + j + "-" + filename;
+                    writeToFile(buffer, outputfile);
+                    buffer.clear();
+                    j++;
+                    i = 0;
+                }
+                buffer.put(line);
+                buffer.put(System.getProperty("line.separator").getBytes(StandardCharsets.UTF_8));
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void createChunksByRowsChars(String inputFile, String outputFolder, int bufferSize, int rows) throws FileNotFoundException, IOException {
+        File f = new File(inputFile);
+        String filename = f.getName();
+        BufferedReader bw = new BufferedReader(new FileReader(f));
+        CharBuffer buffer = CharBuffer.allocate(bufferSize);
+        int j = 0;
+        for (int i = 0; i <= rows; i++) {
+            String lineStr = bw.readLine();
+            // TODO Process each line
+            if (lineStr != null) {
+                char[] line = lineStr.toCharArray();
+                if (i == rows) {
+                    String outputfile = outputFolder + j + "-" + filename;
+                    writeChunk(buffer, outputfile);
+                    buffer.clear();
+                    j++;
+                    i = 0;
+                }
+                buffer.put(line);
+                buffer.put(System.getProperty("line.separator"));
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void writeToFile(char[] c, String outputFile) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter(outputFile))) {
+            bw.write(String.valueOf(c));
+        }
+    }
+
+    public static void writeToFile(byte[] bb, String outputFile) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter(outputFile, "UTF-8"))) {
+            String chunk = new String(bb, StandardCharsets.UTF_8);
+            bw.write(chunk);
+        }
+    }
+
+    public static void writeToFile(ByteBuffer bb, String outputFile) throws IOException {
+        bb.flip();
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter(outputFile, "UTF-8"))) {
+            CharBuffer charBuffer = StandardCharsets.UTF_8.decode(bb);
+            bw.write(charBuffer.toString());
+            charBuffer.clear();
+        }
+    }
+
+    public static void writeChunk(CharBuffer cb, String outputFile) throws IOException {
+        cb.flip();
+        try (BufferedWriter bw = new BufferedWriter(new PrintWriter(outputFile))) {
+            bw.write(cb.toString());
+            cb.clear();
+        }
+    }
+
+    /**
+     * 1.8 readAllLines
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static List<String> readLines(String path) throws IOException {
+        return Files.readAllLines(Paths.get(path));
+    }
+
+    /**
+     * 1.8 readAllLines
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static List<String> readLines(Path path) throws IOException {
+        return Files.readAllLines(path);
+    }
+    /**
+     * Gets the file extension for a given file or throws exception of file not found
+     * @param filepath
+     * @return
+     * @throws FileNotFoundException 
+     */
+    public static String getFileExtension(String filepath) throws FileNotFoundException{
+        File f = new File(filepath);
+        if(f.exists()) {
+            return FilenameUtils.getExtension(filepath);
+        }
+        else {
+            throw new FileNotFoundException();
+        }
+    }
+}
---
+++ src/main/java/fi/johannes/kata/ocr/utils/files/CFolderOperations.java
@@ -0,0 +1,197 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2016 Johannes Sarpola.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fi.johannes.kata.ocr.utils.files;
+
+import fi.johannes.kata.ocr.utils.Logging;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Johannes Sarpola <johannes.sarpola@gmail.com>
+ */
+public class CFolderOperations {
+    /**
+     * files in folder
+     *
+     * @param path
+     * @return
+     */
+    public static File[] getFilesInFolder(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        return listOfFiles;
+    }
+
+    /**
+     * filenames in folder
+     *
+     * @param path
+     * @return
+     */
+    public static List<String> getFilenamesInFolder(String path) {
+        File[] files = getFilesInFolder(path);
+        List<String> names = new ArrayList<>();
+        for (File file : files) {
+            if (file.isFile()) {
+                names.add(file.getName());
+            }
+        }
+        return names;
+    }
+
+    /**
+     * counts files
+     *
+     * @param path
+     * @return
+     */
+    public static int countFiles(String path) {
+        return getFilesInFolder(path).length;
+    }
+
+    /**
+     * Reads all files in folder Java 1.8
+     *
+     * @param folder
+     * @return
+     * @throws IOException
+     */
+    // TODO Maybe add chunk based reading (?)
+    public static LinkedList<List<String>> readAllFilesInFolder(String folder) throws IOException {
+        LinkedList<List<String>> l = new LinkedList<>();
+        Files.walk(Paths.get(folder)).forEach(filePath -> {
+            if (Files.isRegularFile(filePath)) {
+                try {
+                    List<String> lines = CFileOperations.getFileContentAsStrings(filePath);
+                    l.add(lines);
+                } catch (IOException ex) {
+                    Logging.logStackTrace_Error(CFolderOperations.class, ex);
+                }
+            }
+        });
+        return l;
+    }
+
+    /**
+     * Creates folder or returns existing
+     *
+     * @param folder
+     * @return
+     */
+    public static File createFolder(String folder) {
+        if (doesFolderExist(folder)) {
+            return new File(folder);
+        } else {
+            File f = new File(folder);
+            f.mkdir();
+            return f;
+        }
+    }
+
+    public static boolean doesFolderExist(String folder) {
+        File f = new File(folder);
+        return f.isDirectory();
+    }
+    /**
+     * Removes folder and everything inside it
+     * @param folder 
+     */
+    // TODO This doesn't work always, need to be tested
+    public static void recursiveDelete(String folder) {
+        File f = new File(folder);
+        if (f.exists() && f.isDirectory()) {
+            File[] fs = CFolderOperations.getFilesInFolder(folder);
+            for (File fo : fs) {
+                fo.delete();
+                // Back up for deletion upon closing VM
+                if(fo.exists()){
+                    fo.deleteOnExit();
+                }
+            }
+            f.delete();
+            // Back up for deletion upon closing VM
+            if(f.exists()){
+                f.deleteOnExit();
+            }
+        }
+    }
+
+    /**
+     * Creates dir or returns
+     *
+     * @param folder
+     */
+    private static File mkDir(String folder) throws NotDirectoryException {
+        File f = new File(folder);
+        if (!doesFolderExist(folder)) {
+            f.mkdir();
+        }
+        if (f.isDirectory()) {
+            return f;
+        } else {
+            throw new NotDirectoryException(folder);
+        }
+    }
+
+    /**
+     * Merges files from
+     * http://www.programcreek.com/2012/09/merge-files-in-java/
+     *
+     * @param files
+     * @param mergedFile
+     * @throws IOException
+     */
+    public static void mergeFiles(File[] files, File mergedFile) throws IOException {
+
+        FileWriter fstream;
+        BufferedWriter out;
+        fstream = new FileWriter(mergedFile, true);
+        out = new BufferedWriter(fstream);
+        for (File f : files) {
+            FileInputStream fis;
+            fis = new FileInputStream(f);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(fis))) {
+                String aLine;
+                while ((aLine = in.readLine()) != null) {
+                    out.write(aLine);
+                    out.newLine();
+                }
+            }
+
+        }
+        out.close();
+    }
+}
