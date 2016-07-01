0eeb788ab5c21a28c9d82c02a8eefc138fa24c7a
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-07-01 22:13:21 +0300

    Added the main class which should be 'runnable'

---
+++ src/main/java/fi/johannes/kata/ocr/core/OCR.java
@@ -0,0 +1,101 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2016 Johannes Sarpola.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fi.johannes.kata.ocr.core;
+
+import fi.johannes.kata.ocr.cells.CellRows;
+import fi.johannes.kata.ocr.core.data.ApplicationProperties;
+import fi.johannes.kata.ocr.utils.ResourceGetter;
+import fi.johannes.kata.ocr.utils.files.ExistingFileConnection;
+import fi.johannes.kata.ocr.utils.structs.Filename;
+import fi.johannes.kata.ocr.utils.structs.IntegerPair;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author Johannes Sarpola <johannes.sarpola at gmail.com>
+ * @date Jun 25, 2016
+ */
+public class OCR {
+
+    IOManager ioManager;
+    Path input;
+    Path output;
+
+    public OCR() throws IOException {
+        defaultPaths();
+        init();
+
+    }
+
+    public OCR(Path input, Path output) throws IOException {
+        this.input = input;
+        this.output = output;
+        init();
+    }
+
+    private void init() throws IOException {
+        ioManager = new IOManager(input, output);
+        ioManager.connectToInputFiles();
+
+    }
+
+    private void defaultPaths() {
+        input = ResourceGetter.getPath("ocr/");
+        output = Paths.get("./result");
+    }
+
+    public void run() throws FileNotFoundException, IOException {
+        Iterator<Path> pathIter = ioManager.iterator();
+        ioManager.createOutputFolder();
+        
+        IntegerPair cellSize = new IntegerPair(ApplicationProperties.Cells.CELL_WIDTH, ApplicationProperties.Cells.CELL_HEIGHT);
+        CellRows crs;
+        List<OCREntry> entries;
+        List<String> entriesStr;
+        Filename source;
+        
+        while (pathIter.hasNext()) {
+            Path p = pathIter.next();
+            source = new Filename(p);
+            crs = createCellRows(p, cellSize);
+            entries = OCREntryBuilder.buildEntries(crs);
+            entriesStr = OCROutputBuilder.buildOutput(entries);
+            ioManager.writeToFile(source, entriesStr);
+        }
+
+    }
+    public void clearPreviousOutputs(){
+        ioManager.clearOutputfolder();
+        
+    }
+    private CellRows createCellRows(Path p, IntegerPair cellSize) throws FileNotFoundException, IOException {
+        ExistingFileConnection efc = new ExistingFileConnection(p);
+        return new CellRows(cellSize, efc);
+    }
+}
