8b2207d12d318b5fefbffd8eeac4eb3e306625ad
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-07-01 16:08:41 +0300

    Added way to store filename

---
+++ src/main/java/fi/johannes/kata/ocr/utils/structs/Filename.java
@@ -0,0 +1,75 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Johannes Sarpola.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fi.johannes.kata.ocr.utils.structs;
+
+import fi.johannes.kata.ocr.utils.files.CFilePathOperations;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * Caches the relevant infos for a filename
+ * @author Johannes Sarpola
+ * @date Jul 1, 2016
+ */
+class Filename {
+
+    Path absolutePath;
+    String fullpath;
+    String name;
+    String extension;
+
+    public Filename(String filename) throws IOException {
+        if (!CFilePathOperations.validatePath(name)) {
+            absolutePath = Paths.get(filename).toAbsolutePath();
+            this.fullpath = FilenameUtils.getFullPath(absolutePath.toString());
+            this.name = FilenameUtils.getBaseName(absolutePath.toString());
+            this.extension = FilenameUtils.getExtension(absolutePath.toString());
+        } else {
+            throw new IOException();
+        }
+    }
+
+    public String getFilename() {
+        return name + "." + extension;
+    }
+
+    public String getFullpath() {
+        return fullpath;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Path getAbsolutePath() {
+        return absolutePath;
+    }
+    
+}
