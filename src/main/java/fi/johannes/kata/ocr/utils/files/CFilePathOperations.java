3333093713937bd9b251955361a886184c6391a7
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-07-01 15:51:58 +0300

    Added filename validation

--- src/main/java/fi/johannes/kata/ocr/utils/files/CFilePathOperations.java
+++ src/main/java/fi/johannes/kata/ocr/utils/files/CFilePathOperations.java
@@ -23,24 +23,73 @@
  */
 package fi.johannes.kata.ocr.utils.files;
 
+import fi.johannes.kata.ocr.utils.OsUtils;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- *
+ * 
  * @author Johannes
  */
 public class CFilePathOperations {
-    
-    public Path stringToPath(String str) throws FileNotFoundException{
+
+    public static Path stringToPath(String str) throws FileNotFoundException {
         Path p = Paths.get(str);
-        if(Files.exists(p)) return p;
-        else {
+        if (Files.exists(p)) {
+            return p;
+        } else {
             throw new FileNotFoundException();
         }
-        
+
     }
-    
+
+    /**
+     * Validates the filename
+     * @param path
+     * @return
+     */
+    public static boolean validatePath(String path) {
+        try {
+            Paths.get(path);
+
+        } catch (InvalidPathException | NullPointerException ex) {
+            return false;
+        }
+        if (OsUtils.isWindows()) {
+            return validatePathWindows(path);
+        }
+        return true;
+    }
+    /**
+     * http://stackoverflow.com/questions/6730009/validate-a-file-name-on-windows
+     * @author http://stackoverflow.com/users/433790/ridgerunner
+     * @param text
+     * @return 
+     */
+    private static boolean validatePathWindows(String text) {
+        Pattern pattern = Pattern.compile(
+                "# Match a valid Windows filename (unspecified file system).          \n"
+                + "^                                # Anchor to start of string.        \n"
+                + "(?!                              # Assert filename is not: CON, PRN, \n"
+                + "  (?:                            # AUX, NUL, COM1, COM2, COM3, COM4, \n"
+                + "    CON|PRN|AUX|NUL|             # COM5, COM6, COM7, COM8, COM9,     \n"
+                + "    COM[1-9]|LPT[1-9]            # LPT1, LPT2, LPT3, LPT4, LPT5,     \n"
+                + "  )                              # LPT6, LPT7, LPT8, and LPT9...     \n"
+                + "  (?:\\.[^.]*)?                  # followed by optional extension    \n"
+                + "  $                              # and end of string                 \n"
+                + ")                                # End negative lookahead assertion. \n"
+                + "[^<>:\"/\\\\|?*\\x00-\\x1F]*     # Zero or more valid filename chars.\n"
+                + "[^<>:\"/\\\\|?*\\x00-\\x1F\\ .]  # Last char is not a space or dot.  \n"
+                + "$                                # Anchor to end of string.            ",
+                Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.COMMENTS);
+        Matcher matcher = pattern.matcher(text);
+        boolean isMatch = matcher.matches();
+        return isMatch;
+    }
+
 }
