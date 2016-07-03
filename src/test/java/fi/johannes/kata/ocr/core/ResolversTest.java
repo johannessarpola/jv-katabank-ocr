40f74f715d3c07f4a4d38721c09afc564e678ba2
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-07-03 14:39:50 +0300

    For some reason netbeans bugs the imports still

--- src/test/java/fi/johannes/kata/ocr/core/ResolversTest.java
+++ src/test/java/fi/johannes/kata/ocr/core/ResolversTest.java
@@ -25,8 +25,6 @@ package fi.johannes.kata.ocr.core;
 
 import fi.johannes.kata.ocr.cells.Cell;
 import java.util.List;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
